b7765550a11bccfe7e1a280341046dc959526f8b
Liesse205 <liessekm@gmail.com>
2026-04-20 01:17:56 +0200

    Update __init__.py

---
+++ app/services/__init__.py
@@ -0,0 +1,6 @@
+from app.services.auth_service import AuthService
+from app.services.project_service import ProjectService
+from app.services.ledger_service import LedgerService
+from app.services.audit_service import AuditService
+from app.services.risk_service import RiskService
+from app.services.verification_service import VerificationService
