f5c6614acf1748559ad94a6a180f7bf6820d6462
Liesse205 <liessekm@gmail.com>
2026-04-20 01:13:17 +0200

    Update __init__.py

---
+++ app/routes/__init__.py
@@ -0,0 +1,5 @@
+from app.routes.auth import auth_bp
+from app.routes.projects import projects_bp
+from app.routes.donations import donations_bp
+from app.routes.expenses import expenses_bp
+from app.routes.audit import audit_bp
